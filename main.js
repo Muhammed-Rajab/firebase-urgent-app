58ef9e035f7d57f88697ffde1ae1577047936268
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-03-09 00:15:10 +0530

    setup the frontend to send messages

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const messageContainer = document.querySelector(".message-container");
+const messageInput = document.querySelector(".message-input");
+const sendBtn = document.querySelector(".send-btn");
+
+function createMessage(date, message) {
+  const code = `
+        <p class="message" data-id="">
+          <span class="font-bold text-[#6e6e6e]">${date}</span> ${message}
+        </p>
+  `;
+  const temp = document.createElement("template");
+  temp.innerHTML = code.trim();
+
+  return temp.content.firstChild;
+}
